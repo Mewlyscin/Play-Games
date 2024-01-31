62d542d1ef18203f10a4c909b125fc6e7c5a0900
Mewlyscin <Tear-Lyons7@live.fr>
2024-01-31 16:36:26 +0100

    create Footer, logo reseau for assets and router 404 main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,7 @@
+// Import components
+
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 import "./App.css";
 
@@ -6,6 +9,57 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+        lobortis finibus turpis nec fermentum. Proin sodales tincidunt nunc,
+        vitae viverra lacus varius lobortis. Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit. Quisque nulla nisl, mollis id egestas
+        vitae, aliquet viverra tellus. Suspendisse metus risus, elementum ac
+        suscipit gravida, dignissim dictum turpis. Nunc sodales fringilla
+        convallis. In auctor elementum lectus iaculis consequat. Proin vitae
+        ligula non tortor varius posuere vitae sed urna. Interdum et malesuada
+        fames ac ante ipsum primis in faucibus. Etiam faucibus scelerisque
+        turpis, vel iaculis nunc sodales ut. Nunc nec bibendum turpis, quis
+        faucibus diam. Morbi quis nunc non nisi sollicitudin accumsan. Sed
+        gravida, leo sed efficitur interdum, nisi ante porta massa, ac sodales
+        erat ante ac turpis. Aliquam pellentesque bibendum elementum. Sed ac
+        lobortis ligula. Suspendisse potenti. Nunc volutpat sapien dui, sit amet
+        eleifend odio interdum eget. Cras ligula quam, rutrum vitae felis vitae,
+        mattis feugiat ante. Sed id lorem enim. Nullam euismod mauris eget
+        fringilla dignissim. Curabitur ipsum orci, dapibus eget tincidunt eget,
+        dapibus eget nunc. Fusce sit amet diam vel nibh gravida placerat. Ut
+        malesuada aliquam erat, sit amet finibus nibh mattis mattis. Nulla at
+        metus lacinia, lacinia sem ac, condimentum justo. Curabitur egestas eros
+        ante, in interdum metus fermentum a. Integer quis pretium augue.
+        Suspendisse iaculis est eu fermentum feugiat. Lorem ipsum dolor sit
+        amet, consectetur adipiscing elit. Phasellus nibh quam, mattis laoreet
+        sapien quis, volutpat consequat nisl. Nunc tristique tellus nisl, non
+        finibus nunc eleifend eget. Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. In nec arcu et urna eleifend ultricies eget eu sapien.
+        Fusce sed ex tortor. Nullam aliquet volutpat enim, ac fringilla nunc
+        aliquam id. Donec luctus ligula sed felis efficitur auctor. Suspendisse
+        eget justo sed lorem interdum fringilla. Proin a sapien at enim
+        sollicitudin gravida. Quisque aliquet, sem sit amet porta ultrices, ex
+        odio maximus diam, nec convallis dolor leo at nisl. Pellentesque
+        habitant morbi tristique senectus et netus et malesuada fames ac turpis
+        egestas. Morbi efficitur enim vel dolor fermentum, aliquam pharetra orci
+        sodales. Mauris scelerisque gravida lectus, sed faucibus ante lacinia
+        cursus. Donec placerat placerat sollicitudin. Maecenas rutrum ornare leo
+        ac fringilla. Aliquam facilisis condimentum sollicitudin. Ut sit amet
+        cursus ex. Sed consectetur ligula vel ante ultrices, eget viverra elit
+        ornare. Nulla suscipit eros mauris, eu pharetra quam convallis et. Ut
+        pharetra dignissim elit, sed rutrum ipsum. Suspendisse egestas ipsum ac
+        sem malesuada aliquam. Aliquam erat volutpat. Sed ut vulputate felis.
+        Nam elementum tortor vitae leo vulputate, eget sollicitudin libero
+        fringilla. Nullam arcu turpis, tempus elementum lacus a, finibus
+        bibendum arcu. Mauris et elementum nulla. Morbi vel ornare nibh. Ut
+        ullamcorper cursus nisl vel lacinia. Aenean interdum gravida tellus,
+        quis consectetur nunc aliquam quis. Sed egestas ex tellus, a maximus
+        nunc sodales quis. Integer laoreet, ex eu tristique mattis, magna velit
+        feugiat est, a bibendum justo neque sed nisl. Mauris a risus at tortor
+        facilisis elementum.
+      </p>
+      <Footer />
     </div>
   );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
       colors: {
         black: "#000000",
         green: "#7AFE02",
+        beige: "#F3F0EB",
       },
       // backgroundImage: {
       // "bg-header": url("./assets/backgroung_Navbar/logo-Play-games(1).png"),

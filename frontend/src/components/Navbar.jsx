0b6f683121c9794b4f1e11d9e405115f6270bcd3
Mewlyscin <Tear-Lyons7@live.fr>
2024-02-01 00:46:04 +0100

    fix tailwind

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,14 @@
 import { NavLink } from "react-router-dom";
 // import PropTypes from "prop-types";
 
-import backNav from "../assets/background_Navbar/bg_Nav.jpg"; // source : Image de <a href="https://fr.freepik.com/vecteurs-libre/photocall-jeu-au-neon-dessine-main_33634612.htm#query=gaming&position=5&from_view=search&track=sph&uuid=7ec2aef9-fee0-4c96-b967-ffdcac0873ee">Freepik</a>
+import nav from "../assets/nav.jpg"; // source : Image de <a href="https://fr.freepik.com/vecteurs-libre/photocall-jeu-au-neon-dessine-main_33634612.htm#query=gaming&position=5&from_view=search&track=sph&uuid=7ec2aef9-fee0-4c96-b967-ffdcac0873ee">Freepik</a>
 
 // create fonction Navbar
 
 export default function Navbar() {
   return (
     <div>
-      <img className="w-full" src={backNav} alt={backNav} />
+      <img className="w-full" src={nav} alt={nav} />
       <div className="text-3xl font-semibold flex flex-row justify-center align-center">
         <div className="flex flex-col p-7 justify-end">
           <NavLink className="hover:text-green" to="/">
